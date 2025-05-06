1f9dba12efdd8f93a10f2583eee6e07a701e1ef5
Rushel Ekanayaka <127621657+rushela@users.noreply.github.com>
2025-05-06 14:38:51 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -36,8 +36,8 @@ export const HeroSection = () => {
               </svg>
               <img
                 //src="/assets/profile.png"
-                alt="Profile"
-                className="absolute top-1/2 left-1/2 w-48 h-48 md:w-64 md:h-64 object-cover rounded-full -translate-x-1/2 -translate-y-1/2 z-20"
+                //alt="Profile"
+                //className="absolute top-1/2 left-1/2 w-48 h-48 md:w-64 md:h-64 object-cover rounded-full -translate-x-1/2 -translate-y-1/2 z-20"
               />
             </div>
           </div>
